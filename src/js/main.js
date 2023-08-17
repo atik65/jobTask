903dd41f084cb8c6a96306ada16b748db4ce2d5b
atik65 <atikhasan801@gmail.com>
2023-08-17 23:54:15 +0600

    feat: navbar is going on

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import "../styles/style.css";
+import "../styles/style.scss";
+import javascriptLogo from "../../public/images/javascript.svg";
+import logo from "../../public/images/logo.png";
+import viteLogo from "../../public/images/vite.svg";
+import { setupCounter } from "./counter.js";
+
+document.querySelector(".logo").setAttribute("src", logo);
