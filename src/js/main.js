e74a205ece5506edd8c5384c293e08e2c4913104
atik65 <atikhasan801@gmail.com>
2023-08-18 12:45:37 +0600

    feat: search bar added

--- src/js/main.js
+++ src/js/main.js
@@ -3,6 +3,6 @@ import "../styles/style.scss";
 import javascriptLogo from "../../public/images/javascript.svg";
 import logo from "../../public/images/logo.png";
 import viteLogo from "../../public/images/vite.svg";
-import { setupCounter } from "./counter.js";
 
 document.querySelector(".logo").setAttribute("src", logo);
+document.querySelector(".mobileLogo").setAttribute("src", logo);
