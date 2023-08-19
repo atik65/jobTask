d9482c695a064cc3e5bd5670f9a66979f7d5130e
atik65 <atikhasan801@gmail.com>
2023-08-19 13:58:34 +0600

    fix: title added

--- src/js/main.js
+++ src/js/main.js
@@ -4,10 +4,10 @@ import eaLogo from "../../public/images/eaLogo.png";
 import sengoku from "../../public/images/Sengoku.jpg";
 import theSims from "../../public/images/theSims.jpg";
 import remnant from "../../public/images/remnant.jpg";
-import viteLogo from "../../public/images/vite.svg";
 
-const cardsImages = [sengoku, theSims, remnant];
+const gameCardsImages = [sengoku, theSims, remnant];
 
+document.getElementById("title-logo").setAttribute("href", logo);
 document.querySelector(".logo").setAttribute("src", logo);
 
 for (let i = 1; i <= 8; i++) {
@@ -23,7 +23,7 @@ for (let i = 1; i <= 3; i++) {
 for (let i = 1; i <= 3; i++) {
   document
     .querySelector(`#game-card-img${i}`)
-    .setAttribute("src", cardsImages[i - 1]);
+    .setAttribute("src", gameCardsImages[i - 1]);
 }
 
 document.querySelector(".mobileLogo").setAttribute("src", logo);
