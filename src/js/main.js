a41e82af578166bdd48dd98595b0c4f8c7e15e29
atik65 <atikhasan801@gmail.com>
2023-08-19 13:16:02 +0600

    feat: functionalities done

--- src/js/main.js
+++ src/js/main.js
@@ -27,3 +27,68 @@ for (let i = 1; i <= 3; i++) {
 }
 
 document.querySelector(".mobileLogo").setAttribute("src", logo);
+
+// functionalities
+
+const linkButtons = document.getElementsByClassName("menu-link");
+const searchBox = document.getElementById("search-input");
+const searchButton = document.getElementById("searchButton");
+const closeSearchBtn = document.getElementById("search-close");
+const largeMenu = document.getElementsByClassName("large-menu")[0];
+
+let showSearchBox = false;
+let showLargeMenu = false;
+
+// function for handle large menu on click in menu item
+const handleLink = (button) => {
+  const buttonText = button.querySelector("span").innerText;
+
+  if (showLargeMenu == buttonText) {
+    showLargeMenu = false;
+    largeMenu.classList.remove("show-large-menu");
+  } else {
+    showLargeMenu = buttonText;
+    // largeMenu.style.display = "block";
+    largeMenu.classList.add("show-large-menu");
+  }
+
+  // adding bold effect in selected link
+  Array.from(linkButtons).forEach((button) => {
+    const text = button.querySelector("span").innerText;
+    if (text == buttonText && showLargeMenu) {
+      button.style.fontWeight = "bold";
+    } else {
+      button.style.fontWeight = 400;
+    }
+  });
+};
+
+Array.from(linkButtons).forEach((button) => {
+  button.addEventListener("click", () => handleLink(button));
+});
+
+// search button functionalities
+const onSearchBtnClick = () => {
+  if (!showSearchBox) {
+    showLargeMenu = false;
+    showSearchBox = true;
+    largeMenu.classList.remove("show-large-menu");
+    searchBox.classList.add("show-input");
+
+    // removing bold effect in selected link in menu
+    Array.from(linkButtons).forEach((button) => {
+      button.style.fontWeight = 400;
+    });
+  } else {
+    showSearchBox = false;
+  }
+};
+
+// close search button function
+const onCloseSearchBtn = () => {
+  showSearchBox = false;
+  searchBox.classList.remove("show-input");
+};
+
+searchButton.addEventListener("click", onSearchBtnClick);
+closeSearchBtn.addEventListener("click", onCloseSearchBtn);
